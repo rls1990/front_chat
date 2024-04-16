42a26bc4992718dd74bc9ecf423fb440c26dd277
rls1990 <roilanlauzardosotolongo@gmail.com>
2024-04-16 20:25:55 +0200

    new file:   dev-dist/registerSW.js 	new file:   dev-dist/sw.js 	new file:   dev-dist/workbox-9637eeee.js 	modified:   package.json 	new file:   public/chat-192x192.png 	new file:   public/chat-512x512.png 	modified:   vite.config.js 	modified:   yarn.lock

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,44 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestForPlugin = {
+  registerType: "prompt",
+  // includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "ChatPWA",
+    short_name: "MiAppChatPWA",
+    description: "Chat en React con PWA construida con Vite.",
+    theme_color: "#000000",
+    background_color: "#ffffff",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    icons: [
+      {
+        src: "/chat-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/chat-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      ...manifestForPlugin,
+      injectRegister: "auto",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
+});
